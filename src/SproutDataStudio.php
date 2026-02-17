7e2460787b912cb9509aa77cba14365e0a71505c
Ben Parizek <ben.parizek@gmail.com>
2026-02-16 23:01:28 -0500

    Prep release

--- src/SproutDataStudio.php
+++ src/SproutDataStudio.php
@@ -21,6 +21,8 @@ class SproutDataStudio extends Plugin implements SproutPluginMigrationInterface
 
     public string $schemaVersion = '5.0.0';
 
+    public bool $hasCpSection = true;
+
     public static function editions(): array
     {
         return [
